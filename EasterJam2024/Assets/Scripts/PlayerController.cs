60542f44c012f62ccb9bd3c32ae9b368a8c53005
F.H <franklinhu2007@gmail.com>
2024-03-15 11:58:00 -0700

    playermovement

---
+++ EasterJam2024/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Vector2 playerInput;
+    Rigidbody2D rb;
+    public float moveSpeed;
+    float inputHorizontal;
+    bool facingRight = true;
+    private Vector2 pointerInput;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rb.velocity = playerInput.normalized * moveSpeed;
+
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        if (inputHorizontal > 0 && !facingRight) {
+            flip();
+        }
+        if (inputHorizontal < 0 && facingRight) {
+            flip();
+        }
+
+        if (Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0) {
+            anim.SetBool("isRunning", false);
+        } else {
+            anim.SetBool("isRunning", true);
+        }
+    }
+
+    void flip() {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+
+        facingRight = !facingRight;
+    }
+}
