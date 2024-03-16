2f67363631932e39e1177d7c7c195a98554e5130
Jahaanshah Sheikh <wirelesskhurram@outlook.com>
2024-03-16 04:22:16 -0700

    Bow revolves around the center of the player

--- EasterJam2024/Assets/Scripts/WeaponController.cs
+++ EasterJam2024/Assets/Scripts/WeaponController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class WeaponController : MonoBehaviour
 {
     public GameObject arrowPrefab;
-    public Transform bowTransform;
+    public Transform playerTransform;
+    public float distanceFromPlayer = 0.1f;
     public float arrowSpeed = 20f;
 
     private Vector2 direction;
@@ -13,8 +14,6 @@ public class WeaponController : MonoBehaviour
     void Update()
     {
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        direction = (mousePosition - (Vector2)transform.position).normalized;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         bowTransform.rotation = Quaternion.Euler(0, 0, angle);
 
         if (Input.GetMouseButtonDown(0))
@@ -25,9 +24,9 @@ public class WeaponController : MonoBehaviour
 
     void FireArrow()
     {
-        GameObject arrow = Instantiate(arrowPrefab, bowTransform.position, bowTransform.rotation);
+        GameObject arrow = Instantiate(arrowPrefab, transform.position, Quaternion.identity);
         Rigidbody2D rb = arrow.GetComponent<Rigidbody2D>();
-        rb.velocity = direction * arrowSpeed; 
+        rb.velocity = direction * arrowSpeed;
         Destroy(arrow, 7f);
     }
 }
